5913f494533ae0c6174a2e19c7ac3bf40963d35c
wuyue <yy107829@sina.com>
2015-11-25 10:35:29 +0800

    kkkkkk

---
+++ src/com/example/test/BaoDaRenActivity.java
@@ -0,0 +1,16 @@
+package com.example.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaoDaRenActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	public void fuckBao(){
+		
+	}
+}
